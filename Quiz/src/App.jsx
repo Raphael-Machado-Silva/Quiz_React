41a7b405c3ea251865cf8426c77843c2632382ee
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2024-10-31 09:40:56 -0300

    update project Quiz

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,14 +1,23 @@
 import Welcome from './components/Welcome'
+import { QuizContext } from './context/quiz'
+import { useContext, useEffect } from 'react'
+import Question from './components/Question'
 
 import './App.css'
 
 function App() {
+  const [quizState, dispatch ] = useContext(QuizContext)
   
+  useEffect(()=>{
+    //embaralhar as perguntas
+    dispatch({type: "REORDER_QUESTIONS"})
+  }, [])
 
   return (
     <div className="App">
       <h1>Quiz App</h1>
-      <Welcome></Welcome>
+      {quizState.gameStage === "Start" && <Welcome></Welcome>}
+      {quizState.gameStage === "Playing" && <Question></Question>}
     </div>
   )
 }
